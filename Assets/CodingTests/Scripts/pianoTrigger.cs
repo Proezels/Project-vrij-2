13777dbad4e339a5d227933ea4619a5f2204cf48
Proezels <miepsons@gmail.com>
2021-05-19 14:09:31 +0200

    adds audio demos

--- Assets/CodingTests/Scripts/pianoTrigger.cs
+++ Assets/CodingTests/Scripts/pianoTrigger.cs
@@ -7,6 +7,12 @@ public class pianoTrigger : MonoBehaviour
     public GameObject startLight;
     public GameObject fullLight;
     public bool inFront = false;
+    public AudioSource music;
+
+    void Start()
+    {
+        music = gameObject.GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter (Collider other)
     {
@@ -28,6 +34,7 @@ public class pianoTrigger : MonoBehaviour
     {
         if (inFront)
         {
+            music.enabled = !music.enabled;
             startLight.SetActive(false);
             fullLight.SetActive(true);
         }
