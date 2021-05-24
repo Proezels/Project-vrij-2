c4ffc8cb7104aba2e1d23bccdacc0b2b12c54752
Proezels <miepsons@gmail.com>
2021-05-24 19:02:30 +0200

    adds park + fadeout of people + experiments

--- Assets/CodingTests/Scripts/disappear.cs
+++ Assets/CodingTests/Scripts/disappear.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class disappear : MonoBehaviour
 {
-    public GameObject obj;
+    public float fadeIn = 0f; 
+    public Material picnic;
+    public Material park;
+    public bool fading = false;
+    Color colorEnd = Color.gray;
+    float duration = 1f;
+    float startTime;
+    public GameObject fireflies;
+
+    void Start()
+    {
+    //sets starting color to dark green
+        var green = Color.green * 0.5f;
+        park.color = green;
+        startTime = Time.time;
+    }
+    
+
+    void Update()
+    {
+        //sets shader's fade to a float
+        picnic.SetFloat("_fade", fadeIn);
+
+        if (fading)
+        {
+            //fades out shader + changes color of park & disables fireflies
+            fadeIn += Time.deltaTime;
+            float t = (Time.time - startTime) * duration;
+            
+
+            if (fadeIn >= 1f)
+                {
+                    fadeIn = 1f;
+                    fading = false;
+                    park.color = Color.Lerp(park.color, colorEnd, t);
+                    fireflies.SetActive(false);
+                } 
+        }
+    
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
-            obj.SetActive(false);
+            fading = true;
         }
     }
 }
