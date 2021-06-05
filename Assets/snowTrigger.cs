a6403b66b844ab641ff0dd8b7b9649b52dcc51aa
Proezels <miepsons@gmail.com>
2021-06-05 14:59:08 +0200

    living room push

---
+++ Assets/snowTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snowTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
